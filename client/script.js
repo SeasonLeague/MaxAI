81659912996eeb3dd94e0ec546c0170815fb2450
SeasonLeague <facebookups23@gmail.com>
2023-06-16 06:03:40 +0100

    Latest code

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from the server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://maxai-sapm.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
